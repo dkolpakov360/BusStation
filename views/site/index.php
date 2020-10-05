2b47360bf009be59e8e59f591693544deb635550
Dmitry Kolpakov <53349505+dkolpakov360@users.noreply.github.com>
2020-10-05 12:34:37 +0300

    Update index.php

--- views/site/index.php
+++ views/site/index.php
@@ -1,7 +1,2 @@
 <?php
-use app\models\Driver;
-
-$DriverTravelTimeResult = json_encode(Driver::getWithTravelTime($_GET['x1'], $_GET['y1'], $_GET['x2'], $_GET['y2']));
-
-print_r($DriverTravelTimeResult);
 ?>
